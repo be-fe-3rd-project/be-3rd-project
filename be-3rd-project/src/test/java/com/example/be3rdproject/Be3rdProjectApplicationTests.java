13bf237e7fa575a23f222b8a1103530545a93f71
carter-sw <108398461+carter-sw@users.noreply.github.com>
2024-04-17 21:19:32 +0900

    Add files via upload

---
+++ be-3rd-project/src/test/java/com/example/be3rdproject/Be3rdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be3rdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be3rdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
