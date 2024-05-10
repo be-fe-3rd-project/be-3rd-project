4fa3cdc3e699ea645321d9917395040562199d6c
carter-sw <6k2hbc@gmail.com>
2024-05-11 00:42:44 +0900

    5.11 cicd 추가

---
+++ scripts/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ec2-user/spring"
+JAR="$ROOT_PATH/be-3rd-project/be-3rd-project/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar"
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
+
+NOW=$(date +%c)
+
+echo "[$NOW] $JAR 복사" >> $START_LOG
+cp $ROOT_PATH/be-3rd-project/be-3rd-project/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar $JAR
+
+echo "[$NOW] > $JAR 실행" >> $START_LOG
+nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+
+SERVICE_PID=$(pgrep -f $JAR)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
