1221f5f3bc33af061070a92ed5de5dfb3ba3a953
carter-sw <6k2hbc@gmail.com>
2024-05-12 21:59:27 +0900

    start.sh 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ec2-user/spring"
-JAR= "$ROOT_PATH/home/ec2-user/spring/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar # 내 ec2 파일 위치
+JAR="$ROOT_PATH/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar" # 수정된 경로
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
 START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
-
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/home/ec2-user/spring/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
