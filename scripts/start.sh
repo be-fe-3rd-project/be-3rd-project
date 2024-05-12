c890fe5580ae0f51e3d8bd01f509600fa6213ec6
carter-sw <6k2hbc@gmail.com>
2024-05-12 19:22:57 +0900

    복구 내용 커밋

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ec2-user/spring"
-JAR="$ROOT_PATH/be-3rd-project/be-3rd-project/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar" # 내 ec2 파일 위치
+JAR= "$ROOT_PATH/home/ec2-user/spring/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar # 내 ec2 파일 위치
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
 START_LOG="$ROOT_PATH/start.log"
@@ -9,7 +9,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/be-3rd-project/be-3rd-project/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar $JAR
+cp $ROOT_PATH/home/ec2-user/spring/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
