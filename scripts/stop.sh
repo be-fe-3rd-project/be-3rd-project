9caa6b0395f066367c651cc587529d698b523bea
carter-sw <6k2hbc@gmail.com>
2024-05-18 00:56:06 +0900

    set_permissions.sh 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ec2-user/spring"
-JAR="$ROOT_PATH/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar" # 수정된 경로
+JAR="$ROOT_PATH/build/libs/be-3rd-project-0.0.1-SNAPSHOT.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
-SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+SERVICE_PID=$(pgrep -f $JAR) # 실행 중인 Spring 서버의 PID
+
+echo "SERVICE_PID: $SERVICE_PID" >> $STOP_LOG
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NotFound" >> $STOP_LOG
+  echo "[$(date +%c)] 서비스 NotFound" >> $STOP_LOG
 else
-  echo "서비스 종료 " >> $STOP_LOG
+  echo "[$(date +%c)] 서비스 PID: $SERVICE_PID" >> $STOP_LOG
+  echo "[$(date +%c)] 서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
-  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+  if [ $? -eq 0 ]; then
+    echo "[$(date +%c)] 서비스가 정상적으로 종료되었습니다." >> $STOP_LOG
+  else
+    echo "[$(date +%c)] 서비스 종료 실패" >> $STOP_LOG
+  fi
 fi
