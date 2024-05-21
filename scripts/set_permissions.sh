d661be9e1f3f21848bf54e89c32eda566d2a6bdc
carter-sw <6k2hbc@gmail.com>
2024-05-21 21:58:58 +0900

    리뷰 완성

--- scripts/set_permissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo chown -R ec2-user:ec2-user /home/ec2-user/spring
-chmod +x /home/ec2-user/spring/scripts/stop.sh
-chmod +x /home/ec2-user/spring/scripts/start.sh
-chmod 666 /home/ec2-user/spring/stop.log
-chmod 666 /home/ec2-user/spring/start.log
-chmod 666 /home/ec2-user/spring/application.log
-chmod 666 /home/ec2-user/spring/error.log
